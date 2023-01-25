e624a316ca0fdfa6a4ae704a2d34d63680f74d4a
Documenter.jl <documenter@juliadocs.github.io>
2023-01-25 09:49:15 +0000

    build based on 7a0295e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LoKIHelpers","category":"page"},{"location":"#LoKIHelpers","page":"Home","title":"LoKIHelpers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LoKIHelpers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LoKIHelpers]","category":"page"}]
+}
